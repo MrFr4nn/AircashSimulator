d44333053c3e6ea7ff299e44c616b3150d58436e
Nina Salaj <nina.salaj.ns@gmail.com>
2021-12-10 17:13:45 +0100

    dev-sp documentation added

--- AircashSimulator.UI/wwwroot/app/abon_sp/abon_sp.js
+++ AircashSimulator.UI/wwwroot/app/abon_sp/abon_sp.js
@@ -12,16 +12,42 @@ app.config(function ($stateProvider) {
         });
 });
 
-abonSpModule.service("abonSpService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope',
-    function ($http, $q, handleResponseService, config, $rootScope) {
+abonSpModule.service("abonSpService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope', function ($http, $q, handleResponseService, config, $rootScope) {
+    return ({
+        createCoupon: createCoupon
+    });
+    function createCoupon(value, pointOfSaleId) {
+        console.log(config);
+        var request = $http({
+            method: 'POST',
+            url:"https://localhost:44374/api/AbonSalePartner/CreateCoupon",
+            data: {
+                Value: value,
+                PointOfSaleId: pointOfSaleId
+            }
+        });
+        return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
     }
+}
 ]);
 
-abonSpModule.controller("abonSpCtrl",
-    ['$scope', '$state', 'abonSpService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope',
-        function ($scope, $state, abonSpService, $filter, $http, JwtParser, $uibModal, $rootScope) {
-            console.log($rootScope);
-            //$scope.Currency = JwtParser.getProperty('currency');
+abonSpModule.controller("abonSpCtrl",['$scope', '$state', 'abonSpService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope', function ($scope, $state, abonSpService, $filter, $http, JwtParser, $uibModal, $rootScope) {
 
-        }
-    ]);
+    $scope.showCoupon = function () {
+        console.log("test");
+        $("#couponModal").modal("show");
+    }
+
+    $scope.createCoupon = function () {
+        console.log($scope.value);
+        console.log($scope.pointOfSaleId);
+        abonSpService.createCoupon($scope.value, $scope.pointOfSaleId)
+            .then(function (response) {
+                if (response) {
+                    console.log(response);
+                }
+            }, () => {
+                console.log("error");
+            });
+    }
+}]);
