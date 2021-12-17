a37b627c480f988876ab943f148c86f078d0780e
Nina Salaj <nina.salaj.ns@gmail.com>
2021-12-17 15:57:20 +0100

    AircashPayout methods finished and documentation added

---
+++ AircashSimulator.UI/wwwroot/app/aircash_payout/aircash_payout.js
@@ -0,0 +1,149 @@
+﻿var aircashPayoutModule = angular.module('aircashPayout', []);
+
+app.config(function ($stateProvider) {
+    $stateProvider
+        .state('app.aircashPayout', {
+            data: {
+                pageTitle: 'Payout'
+            },
+            url: "/Payout",
+            controller: 'aircashPayoutCtrl',
+            templateUrl: 'app/aircash_payout/aircash_payout.html'
+        });
+});
+
+aircashPayoutModule.service("aircashPayoutService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope', function ($http, $q, handleResponseService, config, $rootScope) {
+    return ({
+        checkUser: checkUser,
+        createPayout: createPayout,
+        checkTransactionStatus: checkTransactionStatus
+    });
+    function checkUser(phoneNumber) {
+        console.log(config);
+        var request = $http({
+            method: 'POST',
+            url: "https://localhost:44374/api/AircashPayout/CheckUser",
+            data: {
+                PhoneNumber: phoneNumber
+            }
+        });
+        return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
+    }
+    function createPayout(phoneNumber, amount) {
+        console.log(config);
+        var request = $http({
+            method: 'POST',
+            url: "https://localhost:44374/api/AircashPayout/CreatePayout",
+            data: {
+                PhoneNumber: phoneNumber,
+                Amount: amount
+            }
+        });
+        return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
+    }
+    function checkTransactionStatus(partnerTransactionId) {
+        console.log(config);
+        var request = $http({
+            method: 'POST',
+            url: "https://localhost:44374/api/AircashPayout/CheckTransactionStatus",
+            data: {
+                PartnerTransactionId: partnerTransactionId
+            }
+        });
+        return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
+    }
+}
+]);
+
+aircashPayoutModule.controller("aircashPayoutCtrl", ['$scope', '$state', 'aircashPayoutService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope', function ($scope, $state, aircashPayoutService, $filter, $http, JwtParser, $uibModal, $rootScope) {
+
+    //console.log($rootScope);
+    $scope.checkUserModel = {
+        phoneNumber: '38512345678'
+    };
+
+    $scope.createPayoutModel = {
+        phoneNumber: '38512345678',
+        amount: 100
+    };
+
+    $scope.checkTransactionStatusModel = {
+        partnerTransactionId: '1C002E8A-98BD-4C75-BA19-87476106DA87'
+    };
+
+    $scope.checkUserServiceBusy = false;
+    $scope.checkUserServiceResponse = false;
+
+    $scope.createPayoutServiceBusy = false;
+    $scope.createPayoutServiceResponse = false;
+
+    $scope.checkTransactionStatusServiceBusy = false;
+    $scope.checkTransactionStatusServiceResponse = false;
+
+    $scope.checkUser = function () {
+        console.log($scope.checkUserModel.phoneNumber);
+        $scope.checkUserServiceBusy = true;
+        aircashPayoutService.checkUser($scope.checkUserModel.phoneNumber)
+            .then(function (response) {
+
+                if (response) {
+                    console.log(response);
+                    $scope.checkUserRequestDateTimeUTC = response.requestDateTimeUTC;
+                    $scope.checkUserResponseDateTimeUTC = response.responseDateTimeUTC;
+                    $scope.checkUserSequence = response.sequence;
+                    response.serviceRequest.signature = response.serviceRequest.signature.substring(0, 10) + "...";
+                    $scope.checkUserResponse = JSON.stringify(response.serviceResponse, null, 4);
+                    $scope.checkUserRequest = JSON.stringify(response.serviceRequest, null, 4);
+                }
+                $scope.checkUserServiceBusy = false;
+                $scope.checkUserServiceResponse = true;
+            }, () => {
+                console.log("error");
+            });
+    }
+
+    $scope.createPayout = function () {
+        console.log($scope.createPayoutModel.phoneNumber);
+        console.log($scope.createPayoutModel.amount);
+        $scope.createPayoutServiceBusy = true;
+        aircashPayoutService.createPayout($scope.createPayoutModel.phoneNumber, $scope.createPayoutModel.amount)
+            .then(function (response) {
+
+                if (response) {
+                    console.log(response);
+                    $scope.createPayoutRequestDateTimeUTC = response.requestDateTimeUTC;
+                    $scope.createPayoutResponseDateTimeUTC = response.responseDateTimeUTC;
+                    $scope.createPayoutSequence = response.sequence;
+                    response.serviceRequest.signature = response.serviceRequest.signature.substring(0, 10) + "...";
+                    $scope.createPayoutResponse = JSON.stringify(response.serviceResponse, null, 4);
+                    $scope.createPayoutRequest = JSON.stringify(response.serviceRequest, null, 4);
+                }
+                $scope.createPayoutServiceBusy = false;
+                $scope.createPayoutServiceResponse = true;
+            }, () => {
+                console.log("error");
+            });
+    }
+
+    $scope.checkTransactionStatus = function () {
+        console.log($scope.checkTransactionStatusModel.partnerTransactionId);
+        $scope.checkTransactionStatusServiceBusy = true;
+        aircashPayoutService.checkTransactionStatus($scope.checkTransactionStatusModel.partnerTransactionId)
+            .then(function (response) {
+
+                if (response) {
+                    console.log(response);
+                    $scope.checkTransactionStatusRequestDateTimeUTC = response.requestDateTimeUTC;
+                    $scope.checkTransactionStatusResponseDateTimeUTC = response.responseDateTimeUTC;
+                    $scope.checkTransactionStatusSequence = response.sequence;
+                    response.serviceRequest.signature = response.serviceRequest.signature.substring(0, 10) + "...";
+                    $scope.checkTransactionStatusResponse = JSON.stringify(response.serviceResponse, null, 4);
+                    $scope.checkTransactionStatusRequest = JSON.stringify(response.serviceRequest, null, 4);
+                }
+                $scope.checkTransactionStatusServiceBusy = false;
+                $scope.checkTransactionStatusServiceResponse = true;
+            }, () => {
+                console.log("error");
+            });
+    }
+}]);
--- AircashSimulator.UI/wwwroot/js/angular-app.js
+++ AircashSimulator.UI/wwwroot/js/angular-app.js
@@ -7,6 +7,7 @@ var app = angular.module('app', [
     'dashboard',
     'abonSp',
     'abonOp',
+    'aircashPayout'
 ]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider)
