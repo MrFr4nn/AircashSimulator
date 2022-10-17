9e8d51e28ce3f743571a34a750e144e6b760ff46
Nikola Tuček <nikola.tucek@vuv.hr>
2022-10-17 11:52:45 +0200

    Aircash Payment commit

---
+++ AircashSimulator/Controllers/AircashPayment/AircashPaymentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using AircashSignature;
+using AircashSimulator.Controllers.AircashPayment;
+using AircashSimulator.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace AircashSimulator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AircashPaymentController : Controller
+    {
+        private AircashConfiguration AircashConfiguration;
+
+        public AircashPaymentController(IOptionsMonitor<AircashConfiguration> aircashConfiguration)
+        {
+            AircashConfiguration = aircashConfiguration.CurrentValue;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAndConfirmPayment(AircashPaymentCreateAndConfirmPayment aircashPaymentCreateAndConfirmPayment)
+        {
+            var dataToVerify = AircashSignatureService.ConvertObjectToString(aircashPaymentCreateAndConfirmPayment);
+            var signature = aircashPaymentCreateAndConfirmPayment.Signature;
+            bool valid = AircashSignatureService.VerifySignature(dataToVerify, signature, $"{AircashConfiguration.AcPayPublicKey}");
+
+            if (valid == true)
+            {
+
+
+
+
+
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Invalid signature");
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ AircashSimulator/Controllers/AircashPayment/AircashPaymentCreateAndConfirmPayment.cs
@@ -0,0 +1,22 @@
+﻿using AircashSignature;
+using System.Collections.Generic;
+
+namespace AircashSimulator.Controllers.AircashPayment
+{
+    public class AircashPaymentCreateAndConfirmPayment : ISignature
+    {
+        public string TransactionId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public List<Parameter> Parameters { get; set; }
+
+        public string Signature { get; set; }
+    }
+
+    public class Parameter
+    {
+        string Key { get; set; }
+        string Value { get; set; }
+    }
+}
