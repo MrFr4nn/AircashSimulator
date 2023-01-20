c0f719b0fc05ebbbeccc3753d7a810842ce7573d
Toni Crnković <toni.crnkovic5@gmail.com>
2023-01-20 14:36:14 +0100

    Cashier added

---
+++ AircashSimulator.UI/wwwroot/app/cashier_abon/cashier_abon.js
@@ -0,0 +1,24 @@
+﻿var cashierAbonModule = angular.module('cashier_abon', []);
+
+app.config(function ($stateProvider) {
+    $stateProvider
+        .state('cashier.cashier_abon', {
+            data: {
+                pageTitle: 'A bon'
+            },
+            url: "/abon",
+            controller: 'cashierAbonCtrl',
+            templateUrl: 'app/cashier_abon/cashier_abon.html'
+        });
+});
+
+cashierAbonModule.service("cashierAbonService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope',
+    function ($http, $q, handleResponseService, config, $rootScope) {
+    }
+]);
+
+cashierAbonModule.controller("cashierAbonCtrl",
+    ['$scope', '$state', 'cashierAbonService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope',
+        function ($scope, $state, cashierAbonService, $filter, $http, JwtParser, $uibModal, $rootScope) {
+        }
+    ]);
