fd74f04233fbb0579c62cea3b49ca038522e983f
Mateo Kostadinović <mateo.kostadinovic@aircash.eu>
2023-02-28 12:53:26 +0100

    up

--- AircashSimulator.UI/wwwroot/app/cashier_ac_frame_ac_pay/cashier_ac_frame_ac_pay.js
+++ AircashSimulator.UI/wwwroot/app/cashier_ac_frame_ac_pay/cashier_ac_frame_ac_pay.js
@@ -14,12 +14,56 @@ app.config(function ($stateProvider) {
 
 cashierAcFrameModule.service("cashierAcFrameAcPayService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope',
     function ($http, $q, handleResponseService, config, $rootScope) {
+        return ({
+            initiateAcFrame: initiateAcFrame
+        });
+
+        function initiateAcFrame(amount) {
+            var request = $http({
+                method: 'POST',
+                url: config.baseUrl + "AircashPayout/Initiate",
+                data: {                    
+                    Amount: amount
+                }
+            });
+            return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
+        }
     }
 ]);
 
 cashierAcFrameModule.controller("cashierAcFrameAcPayCtrl",
     ['$scope', '$state', 'cashierAcFrameAcPayService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope',
         function ($scope, $state, cashierAcFrameAcPayService, $filter, $http, JwtParser, $uibModal, $rootScope) {
-            console.log("ctrl works");
+            $scope.createPayoutModel = {                
+                amount: 100,
+            };
+
+            $scope.createPayoutServiceBusy = false;
+            $scope.createPayoutServiceResponse = false;
+            $scope.initiateAcFrame = function () {
+                $scope.createPayoutServiceBusy = true;
+                cashierAcPayoutService.initiateAcFrame($scope.createPayoutModel.amount)
+                    .then(function (response) {
+                        console.log(response);
+                        if (response.serviceResponse.message == "Unknown phone number") {
+                            $rootScope.showGritter("Error", response.serviceResponse.message);
+                        }
+                        else {
+                            $rootScope.showGritter("Success");
+                        }
+                        $scope.createPayoutServiceBusy = false;
+                        $scope.validateResponded = true;
+                    }, () => {
+                        console.log("error");
+                        $scope.createPayoutServiceBusy = false;
+                    });
+            }
+
+            $scope.setDefaults = function () {                
+                $scope.busy = false;                
+            };
+
+            $scope.setDefaults();
+
         }
     ]);
