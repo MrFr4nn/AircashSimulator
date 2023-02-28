5dc345219ad7c645e44288b27e7d37ca8ee4be2e
Mateo Kostadinović <mateo.kostadinovic@aircash.eu>
2023-02-28 11:54:23 +0100

    up - designe

---
+++ AircashSimulator.UI/wwwroot/app/cashier_ac_frame_ac_pay/cashier_ac_frame_ac_pay.js
@@ -0,0 +1,25 @@
+﻿var cashierAcFrameModule = angular.module('cashier_acFrameAcPay', []);
+
+app.config(function ($stateProvider) {
+    $stateProvider
+        .state('cashier.cashier_acFrameAcPay', {
+            data: {
+                pageTitle: 'Aircash Frame - AcPay'
+            },
+            url: "/aircashFrameAcPay",
+            controller: 'cashierAcFrameAcPayCtrl',
+            templateUrl: 'app/cashier_ac_frame_ac_pay/cashier_ac_frame_ac_pay.html'
+        });
+});
+
+cashierAcFrameModule.service("cashierAcFrameAcPayService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope',
+    function ($http, $q, handleResponseService, config, $rootScope) {
+    }
+]);
+
+cashierAcFrameModule.controller("cashierAcFrameAcPayCtrl",
+    ['$scope', '$state', 'cashierAcFrameAcPayService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope',
+        function ($scope, $state, cashierAcFrameAcPayService, $filter, $http, JwtParser, $uibModal, $rootScope) {
+            console.log("ctrl works");
+        }
+    ]);
