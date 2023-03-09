c3bb0cad839583cb14d2890bea1f2e706d1db74b
Gita Pejović <gita.pejovic@aircash.eu>
2023-03-09 15:13:05 +0100

    In app pay updated

--- AircashSimulator.UI/wwwroot/app/inAppPay/inAppPay.js
+++ AircashSimulator.UI/wwwroot/app/inAppPay/inAppPay.js
@@ -14,11 +14,34 @@ app.config(function ($stateProvider) {
 
 inAppPayModule.service("inAppPayService", ['$http', '$q', 'handleResponseService', 'config', '$rootScope',
     function ($http, $q, handleResponseService, config, $rootScope) {
+        return ({
+            generateTransaction: generateTransaction
+        });
+        function generateTransaction(amount) {
+            var request = $http({
+                method: 'POST',
+                url: config.baseUrl + "AircashPay/GenerateTransaction",
+                data: {
+                    Amount: amount
+                }
+            });
+            return (request.then(handleResponseService.handleSuccess, handleResponseService.handleError));
+        }
     }
 ]);
 
 inAppPayModule.controller("inAppPayCtrl",
     ['$scope', '$state', 'inAppPayService', '$filter', '$http', 'JwtParser', '$uibModal', '$rootScope',
         function ($scope, $state, dashboardService, $filter, $http, JwtParser, $uibModal, $rootScope) {
+            $scope.generateTransactionModel = {
+                amount: 100
+            };
+            $scope.generateTransaction = function () {
+                inAppPayService.generateTransaction()
+                    .then(function (response) {
+                        console.log(response);
+                    });
+            }
         }
     ]);
+
