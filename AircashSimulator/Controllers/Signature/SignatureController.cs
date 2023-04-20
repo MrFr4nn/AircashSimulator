990731e0093a3317926c8708bf4f866ad4f22dba
Toni Crnković <toni.crnkovic5@gmail.com>
2023-04-20 09:43:49 +0200

    Signature validate public key

---
+++ AircashSimulator/Controllers/Signature/SignatureController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircashSimulator.Controllers.Signature
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SignatureController : Controller
+    { 
+        public SignatureController() 
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ValidatePublicKey(ValidatePublicKeyDTO validatePublicKeyDTO) 
+        {
+            byte[] key = Encoding.ASCII.GetBytes(validatePublicKeyDTO.publicKey);
+            X509Certificate2 cert = new X509Certificate2(key);
+            return Ok("Public key valid");
+        }
+    }
+}
