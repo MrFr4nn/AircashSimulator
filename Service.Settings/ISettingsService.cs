bba4fd217923189d19c195bde1b06abd65fb4e8e
ivan.speh <ivan.speh@aircash.eu>
2023-04-20 21:48:55 +0200

    SettingsService added

---
+++ Service.Settings/ISettingsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Settings
+{
+    public interface ISettingsService
+    {
+        Task RefreshSettings();
+        Dictionary<string, string> GetSettings();
+        string AircashSimulatorPrivateKeyPath { get; }
+        string AircashSimulatorPrivateKeyPass { get; }
+    }
+}
