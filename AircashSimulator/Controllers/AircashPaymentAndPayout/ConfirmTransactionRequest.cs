f6adef75b13a23bff8fa46a8c1c86b50a700596d
Gita Pejović <gita.pejovic@aircash.eu>
2023-06-05 12:34:12 +0200

    TopUp and PayOut-added new input fields in Try it out

--- AircashSimulator/Controllers/AircashPaymentAndPayout/ConfirmTransactionRequest.cs
+++ AircashSimulator/Controllers/AircashPaymentAndPayout/ConfirmTransactionRequest.cs
@@ -8,7 +8,9 @@ namespace AircashSimulator.Controllers.AircashPaymentAndPayout
 {
     public class ConfirmTransactionRequest
     {
+        public string PartnerId { get; set; }
         public string BarCode { get; set; }
+        public string PartnerTransactionId { get; set; }
         public string LocationID { get; set; }
         public EnvironmentEnum Environment { get; set; }
     }
