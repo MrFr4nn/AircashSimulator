9eea843553377ebd6de60c0b39112dd6e992f871
Toni Crnkovic <toni.crnkovic5@gmail.com>
2023-06-28 08:44:40 +0200

    Signature service Creating Signature with RSA Private Key

--- Services.Signature/SignatureService.cs
+++ Services.Signature/SignatureService.cs
@@ -8,9 +8,29 @@ using System.Threading.Tasks;
 using DataAccess;
 using Service.Settings;
 using AircashSignature;
+using System.IO;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Asn1.Ocsp;
 
 namespace Services.Signature
 {
+    public class PasswordFinder : IPasswordFinder
+    {
+        private readonly string password;
+
+        public PasswordFinder(string password)
+        {
+            this.password = password;
+        }
+
+        public char[] GetPassword()
+        {
+            return password.ToCharArray();
+        }
+    }
     public class SignatureService: ISignatureService
     {
         private AircashSimulatorContext AircashSimulatorContext;
@@ -22,14 +42,37 @@ namespace Services.Signature
         }
         public string GenerateSignatureFromPemString(string dataToSign, string pem, string certificatePass)
         {
-            var rsa = RSA.Create();
-            rsa.ImportFromEncryptedPem(pem.ToCharArray(), certificatePass);
+            byte[] signeddata;
             var originalData = Encoding.UTF8.GetBytes(dataToSign);
-            using (rsa)
+            try
             {
-                var signeddata = rsa.SignData(originalData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-                return Convert.ToBase64String(signeddata);
+                var rsa = RSA.Create();
+                rsa.ImportFromEncryptedPem(pem.ToCharArray(), certificatePass);
+                using (rsa)
+                {
+                    signeddata = rsa.SignData(originalData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                    return Convert.ToBase64String(signeddata);
+                }
             }
+            catch {
+            
+            }
+            using (var textReader = new StringReader(pem))
+            {
+                var pemReader = new PemReader(textReader, new PasswordFinder(certificatePass));
+
+                AsymmetricCipherKeyPair keyPair = pemReader.ReadObject() as AsymmetricCipherKeyPair;
+                if (keyPair == null)
+                {
+                    throw new InvalidDataException("Failed to read the private key.");
+                }
+                RSAParameters rsaParameters = DotNetUtilities.ToRSAParameters(keyPair.Private as RsaPrivateCrtKeyParameters);
+                var rsa = RSA.Create();
+                rsa.ImportParameters(rsaParameters);
+                signeddata = rsa.SignData(originalData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+            return Convert.ToBase64String(signeddata);
+
         }
 
         public string GenerateSignature(Guid partnerId, string dataToSign)
@@ -68,8 +111,41 @@ namespace Services.Signature
                         result = rsa.VerifyData(dataToVerifyBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
                     }
                 }
+                return result;
             }
             catch
+            {
+                result = false;
+            }
+            try {
+                byte[] signeddata;
+                var bytePublicKey = Encoding.UTF8.GetBytes(validateAndSavePartnerKeyRequest.PublicKey);
+                var certificate = new X509Certificate2(bytePublicKey);
+                var dataToVerifyBytes = Encoding.UTF8.GetBytes("test");
+                using (var textReader = new StringReader(validateAndSavePartnerKeyRequest.PrivateKey))
+                {
+                    var pemReader = new PemReader(textReader, new PasswordFinder(validateAndSavePartnerKeyRequest.Password));
+
+                    AsymmetricCipherKeyPair keyPair = pemReader.ReadObject() as AsymmetricCipherKeyPair;
+                    if (keyPair == null)
+                    {
+                        throw new InvalidDataException("Failed to read the private key.");
+                    }
+                    RSAParameters rsaParameters = DotNetUtilities.ToRSAParameters(keyPair.Private as RsaPrivateCrtKeyParameters);
+                    var rsa = RSA.Create();
+                    rsa.ImportParameters(rsaParameters);
+                    signeddata = rsa.SignData(dataToVerifyBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                }
+                using (var sha256 = new SHA256Managed())
+                {
+                    using (var rsa = certificate.GetRSAPublicKey())
+                    {
+                        result = rsa.VerifyData(dataToVerifyBytes, signeddata, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                    }
+                }
+                return result;
+            }
+            catch 
             {
                 result = false;
             }
@@ -152,4 +228,5 @@ namespace Services.Signature
             return new KeyToSing { PrivateKey = privateKey, PrivateKeyPass = privateKeyPass, PublicKey = publicKey };
         }
     }
+
 }
