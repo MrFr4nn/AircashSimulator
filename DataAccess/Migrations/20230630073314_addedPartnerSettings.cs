24a33346b0cedf35fc61fc2afbd3620aff892a63
Đurica Dakić <durica.dakic@vuv.hr>
2023-06-30 09:41:36 +0200

    New Table for db PartnerSettings

---
+++ DataAccess/Migrations/20230630073314_addedPartnerSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class addedPartnerSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PartnerSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    Parameteres = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartnerSettings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PartnerSettings");
+        }
+    }
+}
---
+++ Domain.Entities/PartnerSettingsEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Domain.Entities.Enum;
+namespace Domain.Entities
+{
+    public class PartnerSettingsEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid PartnerId { get; set; }
+        public PartnerSettingsRoleEnum Role { get; set; }
+        public int Version {get; set; }
+        public string Parameteres { get; set; }
+    }
+}
