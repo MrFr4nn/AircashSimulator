d1d32f60dffaba1bbcecc0c5790a738c277691d8
Nikola Biliš <nikola.bilis@aircash.eu>
2023-07-18 16:23:52 +0200

    Connect Aircash simulator with CreateMultipleCoupon endpoint on EBon

---
+++ AircashSimulator/Controllers/AbonSalePartner/MultipleCouponRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace AircashSimulator.Controllers.AbonSalePartner
+{
+    public class MultipleCouponRequest
+    {
+        public Guid PartnerId { get; set; }
+        public string PointOfSaleId { get; set; }
+        public string ISOCurrencySymbol { get; set; }
+        public string ContentType { get; set; }
+        public int? ContentWidth { get; set; }
+        public List<MultipleCouponCreationRequestDenomination> Denominations { get; set; }
+    }
+
+    public class MultipleCouponCreationRequestDenomination
+    {
+        public decimal Value { get; set; }
+        public string PartnerTransactionId { get; set; }
+    }
+}
